c4b63b116c6784ccb6eff9b615140c261e09b71a
Dimitar Dimitrov <dimitar.dimitrov@hmrc.gov.uk>
2023-01-25 15:49:32 +0000

    reword

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -117,8 +117,8 @@ require('lazy').setup({
 
     -- Colorschemes
     { 'navarasu/onedark.nvim',
-        lazy = false, -- make sure we load this during startup if it is your main colorscheme
-        priority = 1000, -- make sure to load this before all the other start plugins
+        lazy = false,    -- necessary for the main colorscheme
+        priority = 1000, -- necessary for the main colorscheme
         config = function()
             require('plugins/onedark')
         end,
