e81afa1c6fefaf7ccad8985622aa9bda8372b545
Dimitar Dimitrov <mitkofr@yahoo.fr>
2023-02-04 21:47:02 +0000

    Adjustments to styling and wording

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -52,7 +52,7 @@ require('lualine').setup {
             'searchcount',
             { -- LSP server name
                 function()
-                    local msg = 'No Active Lsp'
+                    local msg = 'no LSP'
                     local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
                     local clients = vim.lsp.get_active_clients()
                     if next(clients) == nil then
@@ -61,13 +61,13 @@ require('lualine').setup {
                     for _, client in ipairs(clients) do
                         local filetypes = client.config.filetypes
                         if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
-                            return client.name
+                            return 'LSP: ' .. client.name
                         end
                     end
                     return msg
                 end,
-                icon = ' LSP:',
-                color = { fg = '#ffffff', gui = 'bold' }
+                icon = '',
+                color = { fg = 'cyan' }
             },
             { 'encoding', fmt = function(str) return str:gsub('-', '') end },
             { 'filetype', color = { fg = '#ffab60' } }
