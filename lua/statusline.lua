e5df4067e0fbd8d2663bc82e214a2b5af232fb19
Dimitar Dimitrov <mitkofr@yahoo.fr>
2023-02-05 14:20:38 +0000

    Remove my old statusline#init(), tabs#...() code

--- lua/statusline.lua
+++ lua/statusline.lua
@@ -8,9 +8,4 @@ vim.opt.wildignore:append('*~,*.swp,tags')
 vim.o.wildcharm = '<c-z>' -- cmdline: <c-z> in a mapping acts like <tab>
 
 -- Status line
-vim.o.statusline = "%!statusline#init('❬', '❭')"
 vim.o.laststatus = 2
-
--- Tabline
-vim.o.showtabline = 1
-vim.o.tabline = '%!tabs#MyTabLine()'
