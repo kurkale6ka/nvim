5ee0dafc810a4812a34581e049b32f7af6887dcd
Dimitar Dimitrov <mitkofr@yahoo.fr>
2023-02-08 21:33:11 +0000

    Use a lua table

---
+++ after/plugin/easy-align.lua
@@ -0,0 +1,9 @@
+-- "EasyAlign a" to align arrows
+--     pattern is: one or more 'dashes' followed by >
+--     'a' stands for arrows (I used '>' previously but it conflicts with 'stick_to_left')
+vim.g.easy_align_delimiters = {
+    a = { pattern = [[[-=]\+>]] }
+}
+
+-- nmap ga :'{,'}EasyAlign*
+-- xmap ga :EasyAlign*
