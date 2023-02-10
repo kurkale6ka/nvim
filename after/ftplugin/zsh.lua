e475551b08f2fe8afbb82338bc38ab3da98442a3
Dimitar Dimitrov <dimitar.dimitrov@hmrc.gov.uk>
2023-02-10 11:08:57 +0000

    Temporary fix for zsh/sh snippets conflict

--- after/ftplugin/zsh.lua
+++ after/ftplugin/zsh.lua
@@ -1,1 +1,9 @@
-vim.b.UltiSnipsSnippetDirectories = "['ulti_snippets']"
+-- UltiSnips wrongly detects sh in addition to my zsh snippets.
+-- A *sh.snippets detection must be happening
+-- and that * also matches z, so zsh.snippets
+-- The problem is then that I am always proposed with:
+-- 1. expand from sh snippets
+-- 2. expand from zsh snippets
+-- This is why I moved zsh snippets to their own directory
+-- TODO: raise an issue
+vim.b.UltiSnipsSnippetDirectories = { 'conflictingUltiSnips' }
