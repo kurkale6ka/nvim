9df9b37fc2d7c32d603f8a5a8e55e3ddaca534ea
Dimitar Dimitrov <mitkofr@yahoo.fr>
2023-02-11 17:34:03 +0000

    Beautify

--- after/plugin/fzf.lua
+++ after/plugin/fzf.lua
@@ -1,6 +1,24 @@
-vim.keymap.set('n', '<leader>b', ':Buffers<cr>') -- show all buffers
+-- Fuzzy files
+vim.keymap.set('n', '<leader>b', ':Buffers<cr>')
+vim.keymap.set('n', '<leader>h', ':History<cr>') -- recently edited files
 vim.keymap.set('n', '<leader>l', ':GFiles<cr>') -- ls git files
 vim.keymap.set('n', '<leader>sf', ':silent! Glcd <bar> Files<cr>') -- search fzf files
+vim.keymap.set('n', '<leader>t', ':Filetypes<cr>')
+
+-- Fuzzy help
+vim.keymap.set('n', '<leader>sh', ':Helptags<cr>') -- search help files
+vim.keymap.set('n', 'gh', ':Files ' .. vim.env.XDG_CONFIG_HOME .. '/repos/help<cr>') -- own help files
+
+-- Fuzzy grep
+vim.keymap.set('n', '<leader>/', ':BLines<cr>') -- /fuzzy
+vim.keymap.set('n', '<leader>G', ':BLines <c-r><c-a>') -- :g/fuzzy
+vim.keymap.set('n', '<leader>g', ':silent! Glcd <bar> exe "Rg ".input("ripgrep> ")<cr>') -- ripgrep
+
+vim.keymap.set('n', '<leader>sc', ':Commands<cr>')
+vim.keymap.set('n', '<leader>sd', ':Diagnostics<cr>') -- search LSP diagnostics
+vim.keymap.set('n', '<leader>sm', ':Maps<cr>')
+vim.keymap.set('n', '<leader>ss', ':Snippets<cr>')
+vim.keymap.set('n', '<leader>st', ':Tags<cr>') -- search project tags
 
 -- buffers
 vim.api.nvim_create_user_command('Buffers',
@@ -38,32 +56,17 @@ vim.api.nvim_create_user_command('Files',
     { bang = true, nargs = '?', complete = 'dir', desc = 'fzf files' }
 )
 
-vim.keymap.set('n', '<leader>h', ':History<cr>')   -- search history (recently edited files)
-vim.keymap.set('n', '<leader>sh', ':Helptags<cr>') -- search help files
-vim.keymap.set('n', 'gh', ':Files '..vim.env.XDG_CONFIG_HOME..'/repos/help<cr>') -- own help files
-
-vim.keymap.set('n', '<leader>sd', ':Diagnostics<cr>') -- search diagnostics
-vim.keymap.set('n', '<leader>ss', ':Snippets<cr>')    -- search snippets
-vim.keymap.set('n', '<leader>st', ':Tags<cr>')        -- search tags
-vim.keymap.set('n', '<leader>sc', ':Commands<cr>')    -- search commands
-vim.keymap.set('n', '<leader>sm', ':Maps<cr>')        -- search maps
-
-vim.keymap.set('n', '<leader>t', ':Filetypes<cr>') -- set 'ft
-vim.keymap.set('n', '<leader>/', ':BLines<cr>') -- fuzzy /
-vim.keymap.set('n', '<leader>G', ':BLines <c-r><c-a>') -- fuzzy :g/
-vim.keymap.set('n', '<leader>g', ':silent! Glcd <bar> exe "Rg ".input("ripgrep> ")<cr>') -- ripgrep
-
 -- Rg: ripgrep
 -- TODO: \bpyright regex not working
 vim.api.nvim_create_user_command('Rg',
     function(input)
         vim.fn['fzf#vim#grep'](
             'rg --column --line-number --no-heading --color=always --smart-case --hidden -- ' .. vim.fn.shellescape(input.args),
-            1, -- was --column passed?
+            true, -- column option was passed
             vim.fn['fzf#vim#with_preview'] {
                 options = { '--cycle' }
             },
-            input.bang -- fullscreen?
+            input.bang
         )
     end,
     { bang = true, nargs = '*', desc = 'ripgrep' }
