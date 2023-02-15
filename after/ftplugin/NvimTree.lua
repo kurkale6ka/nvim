67f49a00ffad1323f9a18b0622b7d4b4908bc96b
Dimitar Dimitrov <dimitar.dimitrov@hmrc.gov.uk>
2023-02-15 12:30:19 +0000

    Configure NvimTree

--- after/ftplugin/NvimTree.lua
+++
@@ -1,2 +0,0 @@
-vim.o.statusline = "%{&ft}" -- TODO: bg color from 'onedark'
-vim.wo.fillchars = 'eob: '
--- lua/plugins/NvimTree.lua
+++ lua/plugins/NvimTree.lua
@@ -2,12 +2,15 @@ require("nvim-tree").setup {
     view = {
         mappings = {
             list = {
+                { key = "!", action = "toggle_dotfiles" },
                 { key = "u", action = "dir_up" },
+                { key = "-", action = "" },
             },
         },
     },
     renderer = {
         icons = {
+            git_placement = "signcolumn",
             glyphs = {
                 git = {
                     unstaged = "",
@@ -22,12 +25,23 @@ require("nvim-tree").setup {
         },
     },
     filters = {
+        -- custom = "\\%(^\\.git\\|.\\+\\~\\)$", -- TODO: not working + why is it needed, .git/ is normally excluded
         dotfiles = true,
     },
 }
 
 -- View NvimTree, :Vexplore
-vim.keymap.set('n', '<leader>v', ':NvimTreeFindFileToggle<cr>')
+vim.keymap.set('n', '<leader>v', ':silent! Glcd <bar> NvimTreeFindFileToggle<cr>',
+    { silent = true }
+)
 
--- TODO: nowait?
-pcall(vim.keymap.del, { 'n', 'v' }, '-', { buffer = true, nowait = true })
+vim.o.statusline = "%{&ft}" -- TODO: bg color from 'onedark'
+vim.opt_local.fillchars = { eob = " " }
+
+vim.cmd([[
+highlight NvimTreeNormal guibg=#1f2329 " onedark/palette/darker/bg0
+" highlight link NvimTreeEndOfBuffer NvimTreeNormal " TODO: not working
+highlight NvimTreeEndOfBuffer guibg=#1f2329
+highlight NvimTreeSymlink guifg=cyan
+highlight NvimTreeGitDirty guifg=#af0000 " unstaged
+]])
