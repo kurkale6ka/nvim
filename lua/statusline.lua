8697e38578b8da4cb8df6c5aa98cd76e2074bee1
Dimitar Dimitrov <dimitar.dimitrov@version1.com>
2023-12-19 17:05:35 +0000

    Fix 'wildcharm

--- lua/statusline.lua
+++ lua/statusline.lua
@@ -5,7 +5,8 @@ vim.o.wildmenu = true
 vim.o.wildmode = 'full'
 vim.o.wildignorecase = true
 vim.opt.wildignore:append { '*~', '*.swp', 'tags' }
-vim.o.wildcharm = '<c-z>' -- cmdline: <c-z> in a mapping acts like <tab>
+-- cmdline: <c-z> in a mapping acts like <tab>
+vim.o.wildcharm = 26 -- :lua print((""):byte())
 
 -- Status line
 vim.o.laststatus = 2
