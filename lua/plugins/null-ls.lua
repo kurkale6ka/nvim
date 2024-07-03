35b9e885e10daca97af9e0bc289ad12190a38240
Dimitar Dimitrov <dimitar.dimitrov@version1.com>
2024-07-03 16:41:51 +0100

    isort for python imports

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -12,6 +12,7 @@ local null_ls = require("null-ls")
 
 null_ls.setup {
     sources = {
+        null_ls.builtins.formatting.isort, -- python
         null_ls.builtins.formatting.black, -- python
         null_ls.builtins.formatting.packer,
         -- null_ls.builtins.code_actions.gitsigns,
