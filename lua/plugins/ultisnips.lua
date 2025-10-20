27695098727925e857e3fbc4834b8ff34bdd79b6
Dimitar Dimitrov <kurkale6ka@gmail.com>
2025-10-20 17:44:31 +0100

    Move UltiSnips folder to lua/config

--- lua/plugins/ultisnips.lua
+++ lua/plugins/ultisnips.lua
@@ -1,8 +1,12 @@
+local ulti_snips = vim.fn.stdpath('config') .. '/lua/config/UltiSnips'
+
 return {
     {
         'SirVer/ultisnips',
         dependencies = 'honza/vim-snippets',
         init = function()
+            vim.g.UltiSnipsSnippetDirectories = { 'UltiSnips', ulti_snips }
+            vim.g.UltiSnipsSnippetStorageDirectoryForUltiSnipsEdit = ulti_snips
             vim.g.UltiSnipsEnableSnipMate = false
             vim.g.UltiSnipsListSnippets = '<c-r><tab>'
             vim.g.UltiSnipsExpandTrigger = '<tab>'
