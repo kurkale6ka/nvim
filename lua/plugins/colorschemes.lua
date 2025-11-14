f23a74b5303be1ce7f73d2409ed80737eebd99b2
Dimitar Dimitrov <kurkale6ka@gmail.com>
2025-11-14 12:27:10 +0000

    Use navarasu/onedark to amend a few colors

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -3,6 +3,22 @@ return {
         'olimorris/onedarkpro.nvim',
         priority = 1000,
         opts = {
+            colors = {
+                onedark = {
+                    -- https://github.com/navarasu/onedark.nvim/blob/master/lua/onedark/highlights.lua
+                    bg = '#1f2329', -- palette/darker/bg0
+                    cursorline = '#282c34', -- palette/darker/bg1
+                },
+            },
+            highlights = {
+                CursorLineNR = { fg = '#abb2bf' }, -- fg, highlights.lua: CursorLineNR -> fg
+            },
+            styles = {
+                comments = 'italic',
+                keywords = 'bold,italic',
+                functions = 'italic',
+                conditionals = 'italic',
+            },
             plugins = {
                 all = false,
                 blink_cmp = true,
@@ -13,6 +29,9 @@ return {
                 nvim_tree = true,
                 treesitter = true,
                 trouble = true,
+            },
+            options = {
+                cursorline = true,
             },
         },
     },
